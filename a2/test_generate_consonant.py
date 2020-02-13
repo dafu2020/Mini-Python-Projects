ee1dbac7e755c3f4b5906145b304e05fbdc37b74
cindy <xlu49@my.bcit.ca>
2020-02-13 15:53:51 -0800

    test_generate_consonant.py modified

--- a2/test_generate_consonant.py
+++ a2/test_generate_consonant.py
@@ -5,31 +5,21 @@ import dnd
 
 class Test(TestCase):
     @patch('random.choice', side_effect=['b'])
-    def test_random_b(self, mock_choice):
+    def test_fist_item_on_the_list(self, mock_choice):
         actual = dnd.generate_consonant()
         self.assertEqual(actual, 'b')
 
-    @patch('random.choice', side_effect=['f'])
-    def test_random_f(self, mock_choice):
+    @patch('random.choice', side_effect=['y'])
+    def test_last_item_on_the_list(self, mock_choice):
         actual = dnd.generate_consonant()
-        self.assertEqual(actual, 'f')
+        self.assertEqual(actual, 'y')
 
-    @patch('random.choice', side_effect=['j'])
-    def test_random_j(self, mock_choice):
+    @patch('random.choice', side_effect=['n'])
+    def test_item_in_the_middle_of_the_list_(self, mock_choice):
         actual = dnd.generate_consonant()
-        self.assertEqual(actual, 'j')
-
-    @patch('random.choice', side_effect=['m'])
-    def test_random_m(self, mock_choice):
-        actual = dnd.generate_consonant()
-        self.assertEqual(actual, 'm')
+        self.assertEqual(actual, 'n')
 
     @patch('random.choice', side_effect=['r'])
-    def test_random_r(self, mock_choice):
+    def test_random_item_on_the_list(self, mock_choice):
         actual = dnd.generate_consonant()
         self.assertEqual(actual, 'r')
-
-    @patch('random.choice', side_effect=['x'])
-    def test_random_x(self, mock_choice):
-        actual = dnd.generate_consonant()
-        self.assertEqual(actual, 'x')
