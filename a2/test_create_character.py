91ee3a40805c0aff1d0e8bb3a9f30b71d3116fc6
cindy <xlu49@my.bcit.ca>
2020-02-13 11:26:44 -0800

    test_create_character.py debugged

--- a2/test_create_character.py
+++ a2/test_create_character.py
@@ -3,13 +3,32 @@ from unittest.mock import patch
 import dnd
 
 
-
 class Test(TestCase):
-    @patch('random.choice', side_effect=['a'])
-    def test_random_a(self, mock_choice):
-        actual = dnd.generate_vowel()
-        self.assertEqual(actual, 'a')
 
-    @patch('random.choice', side_effect=['a'])
-    def test_create_character_one_syllable(self):
+    def test_not_valid_input_string(self):
+        actual = 'uyy'
+        dnd.create_character(actual)
+        expected = None
+        self.assertEqual(expected, dnd.create_character(actual))
 
+    def test_not_valid_input_float(self):
+        actual = 2.1
+        dnd.create_character(actual)
+        expected = None
+        self.assertEqual(expected, dnd.create_character(actual))
+
+    @patch('random.choice', side_effect=['b', 'a'])
+    @patch('random.randint', side_effect=[1, 1, 1,
+                                          1, 1, 1,
+                                          1, 1, 1,
+                                          1, 1, 1,
+                                          1, 1, 1,
+                                          1, 1, 1,
+                                          1])
+    @patch('builtins.input', side_effect=['1', '1'])
+    def test_a_character_input_one(self, mock_name, mock_roll, mock_class_race):
+        actual = dnd.create_character(1)
+        expected = {'Name': 'ba', 'Strength': 3, 'Intelligence': 3, 'Wisdom': 3, 'Dexterity': 3, 'Constitution': 3,
+                    'Charisma': 3, 'inventory': [], 'experience points': 0, 'class': 'Barbarian', 'Race': 'Dragonborn',
+                    'HP': [1, 1]}
+        self.assertEqual(expected, actual)
