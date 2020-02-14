9b6a4fd5ce72c797a516a00ec1df06e01a151e04
cindy <xlu49@my.bcit.ca>
2020-02-13 20:17:24 -0800

    test_generate_vowel.py modified

--- a2/test_generate_vowel.py
+++ a2/test_generate_vowel.py
@@ -5,26 +5,16 @@ import dnd
 
 class Test(TestCase):
     @patch('random.choice', side_effect=['a'])
-    def test_random_a(self, mock_choice):
+    def test_first_item_on_the_list(self, mock_choice):
         actual = dnd.generate_vowel()
         self.assertEqual(actual, 'a')
 
-    @patch('random.choice', side_effect=['e'])
-    def test_random_e(self, mock_choice):
+    @patch('random.choice', side_effect=['y'])
+    def test_last_item_on_the_list(self, mock_choice):
         actual = dnd.generate_vowel()
-        self.assertEqual(actual, 'e')
+        self.assertEqual(actual, 'y')
 
     @patch('random.choice', side_effect=['i'])
-    def test_random_i(self, mock_choice):
+    def test_random_item_on_the_list(self, mock_choice):
         actual = dnd.generate_vowel()
         self.assertEqual(actual, 'i')
-
-    @patch('random.choice', side_effect=['o'])
-    def test_random_o(self, mock_choice):
-        actual = dnd.generate_vowel()
-        self.assertEqual(actual, 'o')
-
-    @patch('random.choice', side_effect=['u'])
-    def test_random_u(self, mock_choice):
-        actual = dnd.generate_vowel()
-        self.assertEqual(actual, 'u')
