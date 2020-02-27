73ce1bcae5c8256732b729c33ba634670924103e
cindy <xlu49@my.bcit.ca>
2020-02-27 13:20:05 -0800

    sparse_vector.py and test_sparse_add.py debugged

--- lab05/sparse_vector.py
+++ lab05/sparse_vector.py
@@ -5,39 +5,11 @@ Demonstrate how to calculate the sum and the dot product of two sparse vectors a
 import doctest
 
 
-# store vector as dictionary
-def vector_to_dictionary(sparse_vector=list) -> dict:
-    """ convert sparse vector to dictionary.
-
-    :param sparse_vector: a list contains only number
-    :precondition: sparse_vector must be a list that contains only number
-    :postcondiiton: convert the sparse vector to dictionary form
-    :return: the correctly converted sparse vector as a dictionary
-    >>> sparse_vector = [1]
-    >>> vector_to_dictionary(sparse_vector)
-    {0: 1, 'length': 1}
-    >>> sparse_vector = [1, 0, 0, 1]
-    >>> vector_to_dictionary(sparse_vector)
-    {0: 1, 3: 1, 'length': 4}
-    >>> sparse_vector = [1, 0, 0, 2, 0, 0, 0, 3, 0, 0, 4]
-    >>> vector_to_dictionary(sparse_vector)
-    {0: 1, 3: 2, 7: 3, 10: 4, 'length': 11}
-    """
-    length_vector = len(sparse_vector)
-    my_dic = {}
-    for keys, values in enumerate(sparse_vector):
-        my_dic[keys] = values
-        if my_dic[keys] == 0:
-            my_dic.pop(keys)
-    my_dic['length'] = length_vector
-    return my_dic
-
-
 # add the vector dictionaries
 def sparse_add(vector_one=dict, vector_two=dict) -> dict:
     """Calculate sum.
 
-    A function to calculate the sum of two sparse vectors in dictionary form.
+    A function to calculate the sum of two sparse vectors in dictionary form that only contains integer.
     :param vector_one: a dictionary
     :param vector_two: a dictionary
     :precondition: vector_one and vector_two must be sparse vectors in dictionary form with equal length
@@ -57,18 +29,21 @@ def sparse_add(vector_one=dict, vector_two=dict) -> dict:
     {'length': 11, 2: 3, 4: 2, 6: 1, 9: 1, 7: 3, 10: 4}
     """
     add_dic = {'length': vector_one['length']}
-    if vector_one['length'] == vector_two['length']:
-        for key_one in vector_one:
-            if key_one not in vector_two:
-                add_dic[key_one] = vector_one[key_one]
-            elif key_one in vector_one and key_one != 'length':
-                add_dic[key_one] = vector_one[key_one] + vector_two[key_one]
-                if add_dic[key_one] == 0:
-                    add_dic.pop(key_one)
-        for key_two in vector_two:
-            if key_two not in vector_one:
-                add_dic[key_two] = vector_two[key_two]
-    return add_dic
+    if vector_one['length'] == 0 and vector_two['length'] == 0:
+        return None
+    else:
+        if vector_one['length'] == vector_two['length']:
+            for key_one in vector_one:
+                if key_one not in vector_two:
+                    add_dic[key_one] = vector_one[key_one]
+                elif key_one in vector_one and key_one != 'length':
+                    add_dic[key_one] = vector_one[key_one] + vector_two[key_one]
+                    if add_dic[key_one] == 0:
+                        add_dic.pop(key_one)
+            for key_two in vector_two:
+                if key_two not in vector_one:
+                    add_dic[key_two] = vector_two[key_two]
+        return add_dic
 
 
 # sum the element-wise products of their elements
@@ -95,13 +70,16 @@ def sparse_dot_product(vector_one=dict, vector_two=dict) -> dict:
     2
     """
     dot_product = 0
-    if vector_one['length'] == vector_two['length']:
-        for key_one in vector_one:
-            if key_one in vector_two.keys() and key_one != 'length':
-                dot_product += vector_one[key_one]*vector_two[key_one]
-            else:
-                dot_product += 0
-    return dot_product
+    if vector_one['length'] == 0 and vector_two['length'] == 0:
+        return None
+    else:
+        if vector_one['length'] == vector_two['length']:
+            for key_one in vector_one:
+                if key_one in vector_two and key_one != 'length':
+                    dot_product += vector_one[key_one] * vector_two[key_one]
+                else:
+                    dot_product += 0
+        return dot_product
 
 
 def main():
--- lab05/test_sparse_add.py
+++ lab05/test_sparse_add.py
@@ -10,7 +10,15 @@ class Test(TestCase):
         """Test two empty sparse vectors"""
         argument_one = {'length': 0}
         argument_two = {'length': 0}
-        expected = {'length': 0}
+        expected = None
+        actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
+        self.assertEqual(expected, actual, ' they are two empty vectors')
+
+    def test_length_one_zero_sparse_vector(self):
+        """Test two empty sparse vectors"""
+        argument_one = {'length': 1}
+        argument_two = {'length': 1}
+        expected = {'length': 1}
         actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
         self.assertEqual(expected, actual, ' they are two empty vectors')
 
@@ -46,23 +54,7 @@ class Test(TestCase):
         actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
         self.assertEqual(expected, actual, ' they are two length one vectors with integer elements added to zero')
 
-    def test_sparse_length_one_same_floating_point(self):
-        """Test two length one vectors with same floating point elements"""
-        argument_one = {0: 0.1, 'length': 1}
-        argument_two = {0: 0.1, 'length': 1}
-        expected = {0: 0.2, 'length': 1}
-        actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
-        self.assertEqual(expected, actual, ' they are two length one vectors with same floating point elements')
-
-    def test_sparse_length_one_floating_point_and_integer(self):
-        """Test two length one vectors with same floating point elements"""
-        argument_one = {0: 1, 'length': 1}
-        argument_two = {0: 0.1, 'length': 1}
-        expected = {0: 1.1, 'length': 1}
-        actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
-        self.assertEqual(expected, actual, ' they are two length one vectors with same floating point elements')
-
-    def test_length_more_than_one_same_integer_positive(self):
+    def test_length_more_than_one_same_position_same_integer(self):
         """Test two length more than one sparse vectors with same positive integer elements"""
         argument_one = {0: 1, 1: 1, 'length': 3}
         argument_two = {0: 1, 1: 1, 'length': 3}
@@ -70,7 +62,15 @@ class Test(TestCase):
         actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
         self.assertEqual(expected, actual)
 
-    def test_length_more_than_one_different_integer_positive(self):
+    def test_length_more_than_one_same_position_different_integer(self):
+        """Test two length more than one sparse vectors with same positive integer elements"""
+        argument_one = {0: 1, 1: 2, 2: 3, 'length': 3}
+        argument_two = {0: 4, 1: 5, 2: 6, 'length': 3}
+        expected = {0: 5, 1: 7, 2: 9, 'length': 3}
+        actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
+        self.assertEqual(expected, actual)
+
+    def test_length_more_than_one_different_position_different_integer(self):
         """Test two length more than one sparse vectors with different positive integer elements"""
         argument_one = {1: 2, 'length': 3}
         argument_two = {0: 1, 1: 1, 'length': 3}
@@ -88,24 +88,16 @@ class Test(TestCase):
 
     def test_length_more_than_one_different_integer_pos_and_neg_add_to_zero(self):
         """Test two length more than one sparse vectors with different integer elements added to zero"""
-        argument_one = {1: -2, 'length': 3}
+        argument_one = {1: -2, 2: 1, 'length': 3}
         argument_two = {0: 1, 1: 2, 'length': 3}
-        expected = {0: 1, 'length': 3}
-        actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
-        self.assertEqual(expected, actual)
-
-    def test_length_more_than_one_integer_and_floating_point(self):
-        """Test two length more than one sparse vectors with different integer and floating points"""
-        argument_one = {1: -2, 'length': 3}
-        argument_two = {0: 1, 1: 2.0, 'length': 3}
-        expected = {0: 1, 'length': 3}
+        expected = {0: 1, 2: 1, 'length': 3}
         actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
         self.assertEqual(expected, actual)
 
     def test_length_more_than_one_mixture(self):
         """Test two length more than one sparse vectors with different integer and floating points"""
-        argument_one = {0: 1, 2: 1, 4: 2, 6: 1, 9: 1.34, 'length': 11}
-        argument_two = {0: -1, 2: 2, 7: 3, 10: 4, 'length': 11}
-        expected = {'length': 11, 2: 3, 4: 2, 6: 1, 9: 1.34, 7: 3, 10: 4}
+        argument_one = {0: 8, 2: 1, 4: 2, 6: 1, 9: 1, 'length': 11}
+        argument_two = {0: -8, 2: 2, 6: -3, 10: 4, 'length': 11}
+        expected = {'length': 11, 2: 3, 4: 2, 6: -2, 9: 1, 10: 4}
         actual: dict = sparse_vector.sparse_add(argument_one, argument_two)
         self.assertEqual(expected, actual)
