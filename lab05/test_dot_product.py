b4e737df64a1da11e21c6a22c41a65273b3d3d4e
cindy <xlu49@my.bcit.ca>
2020-02-27 13:20:36 -0800

    test_dot_product.py done

--- lab05/test_dot_product.py
+++ lab05/test_dot_product.py
@@ -10,9 +10,17 @@ class Test(TestCase):
         """Test two empty sparse vectors"""
         argument_one = {'length': 0}
         argument_two = {'length': 0}
+        expected = None
+        actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
+        self.assertEqual(expected, actual, )
+
+    def test_length_one_zero_sparse_vector(self):
+        """Test two length one sparse vectors contains zero"""
+        argument_one = {'length': 1}
+        argument_two = {'length': 1}
         expected = 0
         actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
-        self.assertEqual(expected, actual,)
+        self.assertEqual(expected, actual)
 
     def test_length_one_same_integer_positive(self):
         """Test two length one sparse vectors with same positive integer elements"""
@@ -31,41 +39,41 @@ class Test(TestCase):
         self.assertEqual(expected, actual)
 
     def test_length_one_integer_pos_neg(self):
-        """Test two length one sparse vectors with elements added to zero"""
+        """Test two length one sparse vectors with elements include both positive and negative integers"""
         argument_one = {0: 1, 'length': 1}
         argument_two = {0: -1, 'length': 1}
         expected = -1
         actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
         self.assertEqual(expected, actual)
 
-    def test_length_one_same_floating_point(self):
-        """Test two length one vectors with same floating point elements"""
-        argument_one = {0: 0.1, 'length': 1}
-        argument_two = {0: 0.1, 'length': 1}
-        expected = 0.010000000000000002
+    def test_length_more_than_one_same_position_same_integer(self):
+        """Test two length more than one sparse vector with elements that are at same position and same integer"""
+        argument_one = {0: 1, 1: 2, 2: 1, 'length': 3}
+        argument_two = {0: 1, 1: 2, 2: 1, 'length': 3}
+        expected = 6
         actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
         self.assertEqual(expected, actual)
 
-    def test_length_one_integer_and_floating_point(self):
-        """Test two length one vectors with floating point and integer"""
-        argument_one = {0: 1, 'length': 1}
-        argument_two = {0: 0.1, 'length': 1}
-        expected = 0.1
+    def test_length_more_than_one_same_position_different_integer(self):
+        """Test two length more than one sparse vector with elements that are at same position and different integer"""
+        argument_one = {0: 1, 1: 2, 2: 3, 'length': 3}
+        argument_two = {0: -1, 1: -2, 2: -3, 'length': 3}
+        expected = -14
         actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
         self.assertEqual(expected, actual)
 
-    def test_unique_key(self):
-        """Test two length one vectors with different index elements"""
-        argument_one = {1: 1, 'length': 2}
-        argument_two = {0: 2, 'length': 2}
+    def test_length_more_than_one_different_position(self):
+        """"Test two length more than one sparse vector with elements that are integers at different position"""
+        argument_one = {1: 1, 2: 3, 'length': 3}
+        argument_two = {0: 2, 'length': 3}
         expected = 0
         actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
         self.assertEqual(expected, actual)
 
-    def test_mixture_key(self):
+    def test_length_more_than_one_mixture(self):
         """Test two length one vectors with different properties of the elements"""
-        argument_one = {0: 1, 1: -0.2, 2: -1, 'length': 3}
-        argument_two = {0: 2, 2: 9, 'length': 3}
-        expected = -7
+        argument_one = {0: 1, 1: -2, 2: -1, 3: -1, 'length': 3}
+        argument_two = {0: 2, 2: -9, 3: 6, 'length': 3}
+        expected = 5
         actual: dict = sparse_vector.sparse_dot_product(argument_one, argument_two)
         self.assertEqual(expected, actual)
