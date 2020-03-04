abde522a9896214dc44f7a682173c446fca308a6
cindy <xlu49@my.bcit.ca>
2020-03-04 11:48:55 -0800

    test_make_character.py updated

--- lab06/tests/test_make_character.py
+++ lab06/tests/test_make_character.py
@@ -7,6 +7,18 @@ from unittest.mock import patch
 
 
 class Test(TestCase):
+    @patch('builtins.input', return_value='')
+    def test_empty_name(self, mock_input):
+        expected = {'name': '', 'x': 0, 'y': 0}
+        actual = make_character()
+        self.assertEqual(expected, actual)
+
+    @patch('builtins.input', return_value='c')
+    def test_length_one_name(self, mock_input):
+        expected = {'name': 'C', 'x': 0, 'y': 0}
+        actual = make_character()
+        self.assertEqual(expected, actual)
+
     @patch('builtins.input', return_value='jane')
     def test_make_character(self, mock_input):
         expected = {'name': 'Jane', 'x': 0, 'y': 0}
