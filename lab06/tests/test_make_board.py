f70672ef3da929bfec2cad0233b0eda0370d3d9e
cindy <xlu49@my.bcit.ca>
2020-03-06 17:20:10 -0800

    test_make_board.py updated

--- lab06/tests/test_make_board.py
+++ lab06/tests/test_make_board.py
@@ -6,11 +6,24 @@ from lab06.maze import make_board
 
 
 class Test(TestCase):
-    def test_make_board(self):
+    def test_make_empty_board(self):
+        argument = 0
+        expected = []
+        actual = make_board(argument)
+        self.assertEqual(expected, actual)
+
+    def test_make_1_by_1_board(self):
+        argument = 1
+        expected = [[0, 0]]
+        actual = make_board(argument)
+        self.assertEqual(expected, actual)
+
+    def test_make_5_by_5_board(self):
+        argument = 5
         expected = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4],
                     [1, 0], [1, 1], [1, 2], [1, 3], [1, 4],
                     [2, 0], [2, 1], [2, 2], [2, 3], [2, 4],
                     [3, 0], [3, 1], [3, 2], [3, 3], [3, 4],
                     [4, 0], [4, 1], [4, 2], [4, 3], [4, 4]]
-        actual = make_board()
+        actual = make_board(argument)
         self.assertEqual(expected, actual)
