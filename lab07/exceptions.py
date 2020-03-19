64cf16ef2e64f67cbba02308310963068f7361be
cindy <xlu49@my.bcit.ca>
2020-03-19 00:30:22 -0700

    find_an_even()done

--- lab07/exceptions.py
+++ lab07/exceptions.py
@@ -21,3 +21,25 @@ def heron(my_number) -> float:
         return -1
 
 
+def find_an_even(input_list):
+    """Return the first even number in input_list
+
+     :param input_list: a list of integers
+     :precondition: input_list must be a list of integer
+     :postcondition: return the first even number in input_list
+     :raise ValueError: if input_list does not contain an even number
+     :return: first even number in input_list
+
+     """
+    try:
+        for i in input_list:
+            if i % 2 == 0:
+                return i
+        else:
+            raise ValueError
+    except ValueError:
+        print('wrong list')
+        return -1
+
+
+print(find_an_even([1, 4, 2]))
