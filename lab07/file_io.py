261ea30c2d4ce5830fec9e6806ed9fa16903a9a5
cindy <xlu49@my.bcit.ca>
2020-03-20 03:53:37 -0700

    debugged file_io.py

--- lab07/file_io.py
+++ lab07/file_io.py
@@ -14,15 +14,17 @@ def getText(file_name):
             word_count[word] += 1
         else:
             word_count[word] = 1
-    print(word_count)
+    # print(word_count)
 
     # the sorted nested list = sorting the dictionary by value
     sorted_word_count = sorted([value, key] for (key, value) in word_count.items())
-    print(sorted_word_count)
+    # print(sorted_word_count)
 
     # find the top ten
-    new_list = sorted_word_count[-10:]
-    print(new_list)
+    new_list = []
+    for i in [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10]:
+        new_list.append(sorted_word_count[i])
+    # print(new_list)
 
     # convert the nested list to dictionary form for printing purpose
     final_dic = {}
@@ -30,15 +32,14 @@ def getText(file_name):
         for x in i:
             dic_key = i[1]
             final_dic[dic_key] = i[0]
-    print(final_dic)
+    # print(final_dic)
 
     # print the word:frequency
     for key, value in final_dic.items():
-        print(key, ": ", value)
+        print(key, "-", value)
 
 
 getText('test_io.txt')
 # how to do unittest? any text file?
 # how to sort a dictionary by value
 # reverse the sorted()
-
