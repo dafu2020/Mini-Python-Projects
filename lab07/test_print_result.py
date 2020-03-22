b9506a81e9894b516bedb3cae126483a952fd1b5
cindy <xlu49@my.bcit.ca>
2020-03-22 16:47:21 -0700

    test_print_result.py updated

--- lab07/test_print_result.py
+++ lab07/test_print_result.py
@@ -10,6 +10,7 @@ import file_io
 class Test(TestCase):
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_empty_list(self, mock_stdout):
+        """Test function with a empty list"""
         argument = []
         file_io.print_result(argument)
         expected = ''
@@ -17,19 +18,22 @@ class Test(TestCase):
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_non_nested_size_one_list(self, mock_stdout):
+        """Test function with a non-nested list"""
         argument = ['one', 1]
         with self.assertRaises(TypeError):
             file_io.print_result(argument)
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_nested_size_one_list(self, mock_stdout):
+        """Test function with a size one nested list"""
         argument = [[1, 'one']]
         file_io.print_result(argument)
         expected = 'one - 1\n'
         self.assertEqual(mock_stdout.getvalue(), expected)
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_list_size_less_then_ten_value_all_number(self, mock_stdout):
+    def test_list_size_less_than_ten_value_all_number(self, mock_stdout):
+        """Test function with a size less than ten nested list"""
         argument = [[5, 5], [4, 4], [3, 3], [2, 2], [1, 1]]
         file_io.print_result(argument)
         expected = '5 - 5\n4 - 4\n3 - 3\n2 - 2\n1 - 1\n'
@@ -37,6 +41,7 @@ class Test(TestCase):
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_list_size_less_than_ten_value_all_str(self, mock_stdout):
+        """Test function with a size less than ten nested list with string"""
         argument = [[5, 'five'], [4, 'four'],
                     [3, 'three'], [2, 'two'], [1, 'one']]
         file_io.print_result(argument)
@@ -45,6 +50,7 @@ class Test(TestCase):
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_list_size_ten_value_all_number(self, mock_stdout):
+        """Test function with a size ten nested list with number"""
         argument = [[10, 10], [9, 9], [8, 8], [7, 7], [6, 6], [5, 5], [4, 4], [3, 3], [2, 2], [1, 1]]
         file_io.print_result(argument)
         expected = '10 - 10\n9 - 9\n8 - 8\n7 - 7\n6 - 6\n5 - 5\n4 - 4\n3 - 3\n2 - 2\n1 - 1\n'
@@ -52,6 +58,7 @@ class Test(TestCase):
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_list_size_ten_value_all_str(self, mock_stdout):
+        """Test function with a size ten nested list with string"""
         argument = [[10, 'ten'], [9, 'nine'], [8, 'eight'], [7, 'seven'], [6, 'six'], [5, 'five'], [4, 'four'],
                     [3, 'three'], [2, 'two'], [1, 'one']]
         file_io.print_result(argument)
@@ -60,6 +67,7 @@ class Test(TestCase):
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_list_size_ten_value_str_num_mix(self, mock_stdout):
+        """Test function with a size ten nested list with string and number"""
         argument = [[10, 10], [9, 9], [8, 8], [7, 'seven'], [6, 'six'], [5, 'five'], [4, 'four'],
                     [3, 'three'], [2, 'two'], [1, 'one']]
         file_io.print_result(argument)
@@ -68,6 +76,7 @@ class Test(TestCase):
 
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_list_size_more_than_ten_value_str_num_mix(self, mock_stdout):
+        """Test function with a size more than ten nested list with string and number"""
         argument = [[10, 10], [9, 9], [8, 8], [7, 'seven'], [6, 'six'], [5, 'five'], [4, 'four'],
                     [3, 'three'], [2, 'two'], [1, 'one'], [1, 'new']]
         file_io.print_result(argument)
