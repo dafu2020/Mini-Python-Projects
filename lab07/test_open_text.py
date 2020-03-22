68721f978419ee5ab7731eda473112c86bfbe602
cindy <xlu49@my.bcit.ca>
2020-03-21 17:34:54 -0700

    test_open_text.py created

---
+++ lab07/test_open_text.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+import file_io
+
+
+class Test(TestCase):
+    def test_empty_filename(self):
+        argument = ''
+        with self.assertRaises(FileNotFoundError):
+            file_io.open_text(argument)
+
+    def test_wrong_filename(self):
+        argument = 'this_is_not_existed.txt'
+        with self.assertRaises(FileNotFoundError):
+            file_io.open_text(argument)
+
+    def test_correct_file_name(self):
+        file_name = 'doctest_1.txt'
+        actual = file_io.open_text(file_name)
+        expected = 'this file is for the purpose of doctest:\n'
+        self.assertEqual(expected, expected)
+
--- lab07/test_top_ten_words.py
+++ lab07/test_top_ten_words.py
@@ -23,36 +23,4 @@ class Test(TestCase):
                    'in - 425\nyou - 418\n'
         self.assertEqual(mock_stdout.getvalue(), expected)
 
-        def open_text(file_name: str) -> str:
-            """Open and read file.
-
-            :param file_name: a string
-            :precondition: file_name must be a file
-            :postcondition: open and read a file
-            :return: file opens a str
-
-            """
-            # open file
-            with open(file_name) as file_object:
-                text = file_object.read()
-            return text
-
-        def format_text(text: str) -> str:
-            """Format the text into individual words
-
-            :param text: a string
-            :precondition: file_name must be a file
-            :postcondition: remove punctuation marks; convert all character into lowercase;
-                            split the entire text into individual words
-            :return: the formatted individual words as strings
-            """
-            # replace punctuation mark with ' '(space)
-            for character in ',.~!@#$%^&*()_+-={}[]|/<>:\'\";':
-                text = text.replace(character, '')
-            text = text.lower()
-
-            # get the individual words from the text
-            word_in_text = text.split()
-
-            return word_in_text
 
