79cd39cac74f2da2b7467b1b60f966b187a50b45
cindy <xlu49@my.bcit.ca>
2020-03-21 17:40:55 -0700

    test_top_ten_words.py debugged

--- lab07/test_top_ten_words.py
+++ lab07/test_top_ten_words.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
-import unittest.mock
-import io
+
 import file_io
 
 
@@ -8,19 +7,15 @@ class Test(TestCase):
     def test_empty_filename(self):
         argument = ''
         with self.assertRaises(FileNotFoundError):
-            file_io.top_ten_words(argument)
+            file_io.open_text(argument)
 
     def test_wrong_filename(self):
         argument = 'this_is_not_existed.txt'
         with self.assertRaises(FileNotFoundError):
-            file_io.top_ten_words(argument)
+            file_io.open_text(argument)
 
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def test_alice(self, mock_stdout):
-        file_name = 'alice.txt'
-        file_io.top_ten_words(file_name)
-        expected = 'the - 1804\nand - 912\nto - 797\na - 684\nof - 622\nshe - 538\nit - 529\nsaid - 462\n' \
-                   'in - 425\nyou - 418\n'
-        self.assertEqual(mock_stdout.getvalue(), expected)
-
-
+    def test_alice(self):
+        file_name = 'doctest_1.txt'
+        actual = file_io.open_text(file_name)
+        expected = 'this file is for the purpose of doctest:'
+        self.assertEqual(actual, expected)
