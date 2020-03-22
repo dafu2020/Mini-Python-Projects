2bd58bdaee6d41333f6acb890e53c40211567fc1
cindy <xlu49@my.bcit.ca>
2020-03-22 16:29:15 -0700

    test_open_text.py updated

--- lab07/test_open_text.py
+++ lab07/test_open_text.py
@@ -1,19 +1,25 @@
+"""
+Demonstrate the unittest of open_text function.
+"""
 from unittest import TestCase
 import file_io
 
 
 class Test(TestCase):
     def test_empty_filename(self):
+        """Test function with a empty filename"""
         argument = ''
         with self.assertRaises(FileNotFoundError):
             file_io.open_text(argument)
 
     def test_wrong_filename(self):
+        """Test function with a file that is not existed"""
         argument = 'this_is_not_existed.txt'
         with self.assertRaises(FileNotFoundError):
             file_io.open_text(argument)
 
     def test_right_file(self):
+        """Test function with a file that is existed"""
         file_name = 'doctest_1.txt'
         actual = file_io.open_text(file_name)
         expected = 'this file is for the purpose of doctest:'
