790d7cbfebbf94d19813e068cd7876d34a9f5e34
cindy <xlu49@my.bcit.ca>
2020-03-25 02:50:35 -0700

    tree.py created

---
+++ lab08/tree.py
@@ -0,0 +1,33 @@
+class Tree:
+    def __init__(self, species, age, trunk_circumference):
+        if len(species) > 0:
+            self.species = species.title()
+        else:
+            raise ValueError('The species cannot be empty')
+        if age >= 0:
+            self.age = age
+        else:
+            raise ValueError('The age of the tree cannot be negative')
+        if trunk_circumference >= 0:
+            self.trunk_circumference = trunk_circumference
+        else:
+            raise ValueError('The age of the tree cannot be negative')
+
+    def __str__(self):
+        return f'{self.species} is {self.age} year(s) old, has a trunk circumference of {self.trunk_circumference} ' \
+               f'centimetres.'
+
+    def __repr__(self) -> str:
+        return f'Tree(\"{self.species}\", {self.age}, {self.trunk_circumference})'
+
+
+def main():
+    tree_1 = Tree('Osmanthus fragrans', 12, 20)
+    tree_2 = Tree('Douglas fir', 1, 10)
+
+    print(repr(tree_1))
+    print(tree_1)
+
+
+if __name__ == "__main__":
+    main()
