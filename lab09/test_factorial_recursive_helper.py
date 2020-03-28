a919f6a954dab77c17fd5e1cbf30e0645d027610
cindy <xlu49@my.bcit.ca>
2020-03-27 20:58:16 -0700

    test_factorial_recursive_helper.py

---
+++ lab09/test_factorial_recursive_helper.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from lab09.factorial import factorial_recursive_helper
+
+
+class Test(TestCase):
+    def test_factorial_recursive_helper_0(self):
+        argument = 0
+        actual = factorial_recursive_helper(argument)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_factorial_recursive_helper_1(self):
+        argument = 1
+        actual = factorial_recursive_helper(argument)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_factorial_recursive_helper_bigger_than_1(self):
+        argument = 5
+        actual = factorial_recursive_helper(argument)
+        expected = 120
+        self.assertEqual(expected, actual)
