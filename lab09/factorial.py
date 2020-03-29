d165028fefcf93b060abecc2fb8cfeac77a633a4
cindy <xlu49@my.bcit.ca>
2020-03-29 00:40:06 -0700

    added docstring and doctest to factorial.py

--- lab09/factorial.py
+++ lab09/factorial.py
@@ -1,7 +1,16 @@
 import time
+import doctest
 
 
 def timer(func):
+    """
+    Display the time required for execute a function.
+
+    :param func: a function
+    :precondition: func must be a function
+    :postcondition: write the result of the runtime in file
+    """
+
     def wrapper_timer(*args, **kwargs):
         start_time = time.perf_counter()
         value = func(*args, **kwargs)
@@ -16,17 +25,35 @@ def timer(func):
     return wrapper_timer
 
 
-def write_file(function_name, number, run):
+def write_file(function_name: str, number: int, run: float):
+    """
+    Write in a file.
+
+    :param function_name: a string
+    :param number: an integer
+    :param run: a float
+    :precondition: function_name must be string; number must a integer; run must be a float
+    :postcondition: write in a file with the provided information
+    """
     with open('results.txt', 'a') as file_object:
         file_object.write(f'the run time for {function_name}({number}) is {run} \n')
 
 
 @timer
-def factorial_iterative(my_number: int) -> tuple:
-    """
+def factorial_iterative(my_number: int) -> int:
+    """Calculate factorial using loop
 
-    :param my_number:
-    :return:
+    :param my_number: an integer
+    :precondition: my_number myst ba an integer
+    :postcondition: calculate the factorial using a loop
+    :return: the calculated factorial as an integer
+
+    >>> factorial_iterative(0)
+    1
+
+    >>> factorial_iterative(5)
+    120
+
     """
     factorial = 1
     for i in range(1, my_number + 1):
@@ -37,19 +64,36 @@ def factorial_iterative(my_number: int) -> tuple:
 
 @timer
 def factorial_recursive(my_number: int) -> int:
-    """
+    """Calculate factorial using recursion
 
-    :param my_number:
-    :return:
+    Pass the value of my_number to the factorial_recursive_helper function for actual calculation.
+    :param my_number: an integer
+    :precondition: my_number myst ba an integer
+    :postcondition: calculate the factorial using recursion
+    :return: the calculated factorial as an integer
+
+    >>> factorial_iterative(0)
+    1
+
+    >>> factorial_iterative(5)
+    120
     """
     return factorial_recursive_helper(my_number)
 
 
 def factorial_recursive_helper(num: int) -> int:
-    """s
+    """Calculate factorial using recursion
 
-    :param num:
-    :return:
+    :param num: an integer
+    :precondition: my_number myst ba an integer
+    :postcondition: calculate the factorial using recursion
+    :return: the calculated factorial as an integer
+
+    >>> factorial_iterative(0)
+    1
+
+    >>> factorial_iterative(5)
+    120
     """
 
     if num <= 1:
@@ -58,6 +102,11 @@ def factorial_recursive_helper(num: int) -> int:
 
 
 def main():
+    """
+    Drive program
+    """
+    doctest.testmod()
+
     for i in range(1, 101):
         (iterative_value, iterative_name, iterative_runtime) = factorial_iterative(i)
         (recursive_value, recursive_name, recursive_runtime) = factorial_recursive(i)
