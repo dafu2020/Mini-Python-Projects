2166813d9f51b2817cb1ad961d0cc47219f1ca4f
cindy <xlu49@my.bcit.ca>
2020-03-29 01:06:09 -0700

    added docstring and doctest to calories.py

--- lab09/calories.py
+++ lab09/calories.py
@@ -1,38 +1,60 @@
-def add_calories_dictionary(user_input: str) -> dict:
-    """Build a dictionary for food and it's calories.
+# start with a empty dict?
+# both input in main?
+def add_calories_dictionary(new_food: str) -> dict:
+    """Build a dictionary for food and it's calories
 
-    :param user_input: a string
-    :precondition:
-    :postcondition:
-    :return:
+    :param new_food: a string
+    :precondition: new_food must be a string
+    :postcondition: update the calories_dictionary with new food and the corresponding calories
+    :return: the updated calories_dictionary as a dictionary
     """
-    calories_dict = {"lettuce": 5, "carrot": 52, "apple": 72, "bread": 66,
-                     "pasta": 221, "rice": 225, "milk": 122, "cheese": 115,
-                     "yogurt": 145, "beef": 240, "chicken": 140, "butter": 102
-                     }
-    new_food = user_input
-    new_calories = int(input(f'Please enter the calorie of the {new_food}: '))
-    calories_dict[new_food] = new_calories
+    try:
+        calories_dict = {"lettuce": 5, "carrot": 52, "apple": 72, "bread": 66,
+                         "pasta": 221, "rice": 225, "milk": 122, "cheese": 115,
+                         "yogurt": 145, "beef": 240, "chicken": 140, "butter": 102
+                         }
+        new_calories = int(input(f'Please enter the calorie of the {new_food}: '))
+        calories_dict[new_food] = new_calories
+    except ValueError:
+        print('Please enter a integer for calorie')
+
     return calories_dict
 
 
 def food_name_list(food_dict: dict) -> list:
-    """
+    """Create a list containing food names
 
-    :param food_dict:
-    :return:
+    :param food_dict: must be a dict
+    :precondition: food_dict must be a dict
+    :postcondition: store all the name of the food in the dictionary in a list
+    :return: a list containing all food names from the food dictionary
+
+    >>> food_dict_1 = {'peach':10, 'apple':20}
+    ['apple', 'peach']
+
+    >>> food_dict_2 = {"carrot": 5, "apple": 5, "bread": 5]
+    []
     """
-    food_list = []
-    for key in food_dict:
-        food_list.append(key)
+    food_list = [item for item in food_dict]
     return sorted(food_list)
 
 
 def calculate_total_calorie(food_dict: dict) -> int:
-    """
+    """Calculate total calories
 
-    :param food_dict:
-    :return:
+    :param food_dict: a dict
+    :precondition: food_dict must be a dictionary
+    :postcondition: calculate the total calorie of a food dictionary
+    :return: the calculated total calorie as an integer
+
+    >>> food_dict_1 = {'peach':10, 'apple':20}
+    >>> calculate_total_calorie(food_dict_1)
+    30
+
+    >>> food_dict_2 = {"carrot": 5, "apple": 5, "bread": 5]
+    >>> calculate_total_calorie(food_dict_2)
+    15
+
     """
     total_calories = 0
     for item in food_dict:
@@ -43,8 +65,18 @@ def calculate_total_calorie(food_dict: dict) -> int:
 def calculate_calorie_average(food_dict: dict) -> int:
     """Calculate the average calorie
 
-    :param food_dict:
-    :return:
+    :param food_dict: a dict
+    :precondition: food_dict must be a dictionary
+    :postcondition: calculate the average calorie of a food dictionary
+    :return: the calculated average calorie as an integer
+
+    >>> food_dict_1 = {'peach':10, 'apple':20}
+    >>> calculate_calorie_average(food_dict_1)
+    15
+
+    >>> food_dict_2 = {"carrot": 5, "apple": 5, "bread": 5]
+    >>> calculate_calorie_average(food_dict_2)
+    5
     """
     total_calories = calculate_total_calorie(food_dict)
     average_calories = total_calories / len(food_dict)
@@ -52,6 +84,11 @@ def calculate_calorie_average(food_dict: dict) -> int:
 
 
 def main():
+    """
+    Drive the program.
+    """
+    doctest.testmode()
+
     while True:
         user_input = input("Enter food item to add, or 'q' to exit: ").lower().strip()
         if user_input == 'q':
