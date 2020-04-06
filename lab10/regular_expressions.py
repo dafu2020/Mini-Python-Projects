9b07a4d08a61a71e699325814a6f418133bd050b
cindy <xlu49@my.bcit.ca>
2020-04-05 22:46:03 -0700

    minor update

--- lab10/regular_expressions.py
+++ lab10/regular_expressions.py
@@ -32,7 +32,7 @@ def is_email(address: str) -> bool:
 
 
 def is_nakamoto(name: str) -> bool:
-    """ Check if a name has a last name of Nakamoto
+    """Check if a name has a last name of Nakamoto
 
     :param name: a string
     :precondition: name must be a string
@@ -60,7 +60,7 @@ def is_poker(hand: str) -> bool:
     :param hand: a string
     :precondition: hand must be a five character string
     :postcondition: validate a poker hand
-    :return: if a poker hand is valid return True, else return False
+    :return: if a poker hand is invalid return False, else return True
 
     >>> is_poker('aaaaa')
     False
@@ -70,19 +70,9 @@ def is_poker(hand: str) -> bool:
     # can find invalid pattern and return False, else return True
     poker_regex = re.compile(r'\w{6,}'  # card set cannot be more than 5 cards
                              r'|^\w{,4}$'  # card set cannot be less than 5 cards
-                             r'|[^2-9akqtj]'  # card set cannot contain invalid cards other than 2-9, a, k, q, t
-                             r'|a{5}'   # if same card more than 5 is also consider invalid
-                             r'|k{5}'
-                             r'|q{5}'
-                             r'|t{5}'
-                             r'|2{5}'
-                             r'|3{5}'
-                             r'|4{5}'
-                             r'|5{5}'
-                             r'|6{5}'
-                             r'|7{5}'
-                             r'|8{5}'
-                             r'|9{5}', re.I)    # case in-sensitive
+                             r'|[^2-9akqjt]'  # card set cannot contain invalid cards other than 2-9, a, k, q, t
+                             r'|([2-9akqjt])\1{4}', re.I)  # if same card more than 5 is also consider invalid
+    # case in-sensitive
     match_object = poker_regex.search(hand)
     if match_object:
         return False
@@ -93,8 +83,19 @@ def main():
     """
     Drive the program
     """
-    doctest.testmod
-    # print(is_poker('qqqqj'))
+    doctest.testmod()
+
+    # check email
+    user_email = 'jane_doe@gmail.com'
+    print('check email: ', is_email(user_email))
+
+    # check name
+    user_name = 'Alice Nakamoto'
+    print('check name: ', is_nakamoto(user_name))
+
+    # check poker
+    poker_hand = 'aaa88'
+    print('check poker: ', is_poker(poker_hand))
 
 
 if __name__ == "__main__":
