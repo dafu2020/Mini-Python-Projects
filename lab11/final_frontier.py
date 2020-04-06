0fbda76474e9bfd0f7ab61d1d4b95751d9c61464
cindy <xlu49@my.bcit.ca>
2020-04-05 22:17:12 -0700

    final_frontier.py added docstring

--- lab11/final_frontier.py
+++ lab11/final_frontier.py
@@ -5,7 +5,14 @@ import requests
 import json
 
 
-def get_mars_info(key):
+def get_mars_info(key: str) -> object:
+    """Fetch Mars weather information by using NASA API
+
+    :param key: a str
+    :precondition: key must be a str
+    :postcondition: fetch Mars weather information and store in a json object
+    :return: the fetched Mars weather information as a json object
+    """
     url = f'https://api.nasa.gov/insight_weather/?api_key={key}&feedtype=json&ver=1.0'
     response = requests.get(url)
     response.raise_for_status()
@@ -13,7 +20,16 @@ def get_mars_info(key):
     return mars_info
 
 
-def print_mars_info(mars, sol):
+def print_mars_info(mars: object, sol: str) -> None:
+    """Print weather information
+
+    Print the season and average atmospheric temperature of a sol on Mars
+    :param mars: a json object
+    :param sol: a str
+    :precondition: mars must be a json object containing weather information on Mars fetched by NASA API;
+                    sol must be a str representing a sol on Mars
+    :postcondition: print the season and average atmospheric temperature of a sol on Mars
+    """
     s = mars[sol]
     # get season
     season = s['Season']
@@ -31,11 +47,12 @@ def main():
     # the list  of sol NASA API provided
     sol_list = ['475', '476', '477', '478', '479', '480', '481']
 
+    # fetch the information about the summer and average atmospheric temperature every 5 min
     while True:
         sol = random.choice(sol_list)
         mars_weather = get_mars_info(key)
-
         print_mars_info(mars_weather, sol)
+        # 300 s = 5 min
         time.sleep(300)
 
 
