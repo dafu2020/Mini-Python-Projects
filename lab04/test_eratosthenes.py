c491ba639f72a1cdbc9f8f83ab6e009ab809a0be
cindy <xlu49@my.bcit.ca>
2020-04-12 23:17:15 -0700

    grammar fix

--- lab04/test_eratosthenes.py
+++ lab04/test_eratosthenes.py
@@ -18,7 +18,7 @@ class Test(TestCase):
         self.assertEqual(expected, lab04.eratosthenes(argument),
                          "The upperbound number is the smallest positive integer, one. ")
 
-    def test_eratosthenes_two(self):
+    def test_eratosthenes_smallest_upperbound(self):
         """Test number two, the upperbound number is the smallest prime number. """
         argument = 2
         expected = [2]
@@ -26,7 +26,7 @@ class Test(TestCase):
         self.assertEqual(expected, lab04.eratosthenes(argument),
                          "The upperbound number is the smallest prime number. ")
 
-    def test_eratosthenes_thirty_one(self):
+    def test_eratosthenes_prime_upperbound(self):
         """Test number thirty one, the upper-bond number that is a primer number."""
         argument = 31
         expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
@@ -34,7 +34,7 @@ class Test(TestCase):
         self.assertEqual(expected, lab04.eratosthenes(argument),
                          "The upperbound number is a prime number.")
 
-    def test_eratosthenes_one_hundred(self):
+    def test_eratosthenes_non_prime_upperbound(self):
         """Test number one hundred, the upper-bond number that is a non-primer number."""
         argument = 100
         expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
--- lab04/test_is_prime_number.py
+++ lab04/test_is_prime_number.py
@@ -11,15 +11,15 @@ class Test(TestCase):
         self.assertEqual(expected, lab04.is_prime_number(argument),
                          "The number is zero.")
 
-    def test_is_prime_number_smallest_integer(self):
-        """Test the smallest integer."""
+    def test_is_prime_number_smallest_positive_integer(self):
+        """Test the smallest positive integer."""
         argument = 1
         expected = False
         lab04.is_prime_number(argument)
         self.assertEqual(expected, lab04.is_prime_number(argument),
                          "The number is the smallest positive integer.")
 
-    def test_is_prime_number_two(self):
+    def test_is_prime_number_smallest_prime(self):
         """Test integer two, the smallest prime number."""
         argument = 2
         expected = True
@@ -27,7 +27,7 @@ class Test(TestCase):
         self.assertEqual(expected, lab04.is_prime_number(argument),
                          "The number two, the smallest prime number")
 
-    def test_is_prime_number_thirty_one(self):
+    def test_is_prime_number_prime_bigger_than_10(self):
         """Test number thirty one, a prime number larger than 10."""
         argument = 31
         expected = True
@@ -35,7 +35,7 @@ class Test(TestCase):
         self.assertEqual(expected, lab04.is_prime_number(argument),
                          "The number thirty one, a prime number larger than 10")
 
-    def test_is_prime_number_one_hundred(self):
+    def test_is_prime_number_non_prime_bigger_than_10(self):
         """Test number one hundred, a non-prime number larger than 10."""
         argument = 100
         expected = False
